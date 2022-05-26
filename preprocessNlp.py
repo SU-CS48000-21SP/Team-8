fa2a9c6557f84a36f12c390ba55ad4f715db78c8
Deniz Gunes <33930666+denizgns@users.noreply.github.com>
2022-05-26 23:26:27 +0300

    Update preprocessNlp.py

--- preprocessNlp.py
+++ preprocessNlp.py
@@ -4,33 +4,37 @@ from nltk.tokenize import word_tokenize
 import csv
 from csv import reader
 import re
+
 #Extracts all the query titles and deletes stopwords for semantic similarity analysis
+
+def preProcessFile(inputFile, filteredFile):
+    stop_words = set(stopwords.words('english'))
+    add_stopword = ['python', 'how', 'to', 'show', 'shows', 'showing']
+    # open query dataset in read mode
+    # with open("StackOverflow_Python_Scraping\python_queries.csv", "r", encoding= "utf-8") as f:
+    with open(inputFile, "r", encoding= "utf-8") as f:
+        reader = csv.reader(f, delimiter=",")
+        for i, line in enumerate(reader):
+            #extracting query titles
+            contentlist ='line[{}] = ({}'.format(i, line)
+            title= contentlist.split(",")[11]
+
+            #preparing data
+            title = re.sub(r'[^\w\s]', '', title)
+            title = title.lower()
+            word_tokens = word_tokenize(title)
+           
+            #writing filtered queries to a new txt file
+            filtered = [w for w in word_tokens if not (( w in stop_words) or ( w in add_stopword))] 
+            #print(filtered)
+            #f = open("filteredNew.csv", "a")
+            f = open(filteredFile, "a")
+            line = ''
+            for word in filtered:
+                line += word + ' '
+            f.write( line +",")
+            f.close()
+            line = ''
+
 print("Extracting Queries For NLP Preprocessing")
-
-stop_words = set(stopwords.words('english'))
-
-# open query dataset in read mode
-with open("python_queries.csv", "r", encoding= "utf-8") as f:
-    reader = csv.reader(f, delimiter=",")
-    for i, line in enumerate(reader):
-        #extracting query titles
-        contentlist ='line[{}] = ({}'.format(i, line)
-        title= contentlist.split(",")[11]
-
-        #preparing data
-        title = re.sub(r'[^\w\s]', '', title)
-        title = title.lower()
-        word_tokens = word_tokenize(title)
-        #Writing the data to txt file
-        f = open("Titles.txt", "a")
-        f.write(title + "\n")
-        f.close()
-        #writing filtered queries to a new txt file
-        filtered = [w for w in word_tokens if not w in stop_words] 
-        #f = open("filteredTitles.txt", "a")
-        # line = ''
-        # for word in filtered:
-        #     line += word + ' '
-        # f.write( line +"\n")
-        # f.close()
-        # line = ''
+preProcessFile("StackOverflow_Python_Scraping\python_queries.csv", "filteredDeneme.csv")
